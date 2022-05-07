3dd45a3f5c6a173a3fc2eb2a74440e9da27c105b
Honsberg Andre <floridait@gmail.com>
2022-05-07 19:54:15 +0200

    switched to string

--- dart_go_com.go
+++ dart_go_com.go
@@ -8,13 +8,12 @@ package dart_go_com
 // bool GoDart_PostCObject(Dart_Port_DL port, Dart_CObject* obj) {
 //   return Dart_PostCObject_DL(port, obj);
 // }
-import "C"
-import "unsafe"
 
-type Trans struct {
-	channel string
-	payload string
-}
+import (
+	"C"
+	"fmt"
+	"unsafe"
+)
 
 func Init(api unsafe.Pointer) {
 	if C.Dart_InitializeApiDL(api) != 0 {
@@ -26,10 +25,11 @@ func Init(api unsafe.Pointer) {
 func SendToPort(port int64, channel string, payload string) {
 	var obj C.Dart_CObject
 	obj._type = C.Dart_CObject_kInt64
-	data := &Trans{}
-	data.channel = channel
-	data.payload = payload
-	unsafeM := (*int64)(unsafe.Pointer(data))
+
+	dataTpl := "{\"channel\":\"%s\",\"payload\":\"%s\"}"
+	data := fmt.Sprintf(dataTpl, channel, payload)
+	unsafeM := (*int64)(unsafe.Pointer(&data))
+
 	*(*C.int64_t)(unsafe.Pointer(&obj.value[0])) = C.int64_t(*unsafeM)
 	C.GoDart_PostCObject(C.int64_t(port), &obj)
 	data = nil
