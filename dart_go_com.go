158f84d161a2f3612b205ee6237514063b1fc0b8
Honsberg Andre <floridait@gmail.com>
2022-05-08 20:44:14 +0200

    got it nice now

--- dart_go_com.go
+++ dart_go_com.go
@@ -1,7 +1,8 @@
 package dart_go_com
 
+// #include "stdlib.h"
 // #include "stdint.h"
-// #include "string.h"
+// #include "stdio.h"
 // #include "include/dart_api_dl.c"
 //
 // // Go does not allow calling C function pointers directly. So we are
@@ -21,13 +22,20 @@ package dart_go_com
 //      int64_t ptr = (int64_t)pCom;
 //      return ptr;
 // }
+//
+// void clearComStructMemory(ComObj pCom) {
+//      free(&pCom.data);
+// }
 
 import "C"
-import "unsafe"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func Init(api unsafe.Pointer) {
 	if C.Dart_InitializeApiDL(api) != 0 {
-		panic("failed to initialize Dart DL C API: version mismatch. " +
+		fmt.Errorf("failed to initialize Dart DL C API: version mismatch. " +
 			"must update include/ to match Dart SDK version")
 	}
 }
@@ -35,9 +43,17 @@ func Init(api unsafe.Pointer) {
 func SendToPort(port int64, data string) {
 	var obj C.Dart_CObject
 	obj._type = C.Dart_CObject_kInt64
+
 	var pcom unsafe.Pointer
 	ptrAddr := C.GetCom(&pcom, C.CString(data))
+
 	*(*C.int64_t)(unsafe.Pointer(&obj.value[0])) = ptrAddr
-	C.GoDart_PostCObject(C.longlong(port), &obj)
-	defer C.free(pcom)
+
+	C.GoDart_PostCObject(C.int64_t(port), &obj)
+
+}
+
+func FreeComStructMemory(pointer *int64) {
+	ptr := (*C.struct_ComObj)(unsafe.Pointer(pointer))
+	C.clearComStructMemory(*ptr)
 }
